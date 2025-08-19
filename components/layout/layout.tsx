e60fd9f230a8d189f54a21c37fbd354cbce8b0f1
shirokivgromada1 <150667040+shirokivgromada1@users.noreply.github.com>
2025-08-19 19:03:24 +0200

    correct favicon path

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -64,7 +64,7 @@ export const Layout = ({
             name="google-site-verification"
             content="NpwI4zB8WE9vLL8fRonI6tLDEyvx3A5daysl-f0FZiI"
           />
-          <link rel="icon" href="./../../public/favicon.ico" sizes="32x32" />
+          <link rel="icon" href="favicon.ico" sizes="32x32" />
         </Head>
         <Header
           data={data?.header}
